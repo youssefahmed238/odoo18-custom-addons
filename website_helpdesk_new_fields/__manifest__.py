bc513b7e5daba12d9252b02ddb95a3bcdee10055
Youssef Ahmed <youssef01120646586@gmail.com>
2025-09-02 19:58:03 +0300

    initiate manifest file

---
+++ website_helpdesk_new_fields/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Website Helpdesk New Fields',
+    'author': 'Youssef',
+    'category': 'Website',
+    'version': '18.0.0.1.0',
+    'depends': ['website', 'website_helpdesk'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
