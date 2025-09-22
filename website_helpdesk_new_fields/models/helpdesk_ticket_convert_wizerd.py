a4ce5bb011f3fa2c7ec26cf702e280a57a3cf76d
Youssef Ahmed <youssef01120646586@gmail.com>
2025-09-23 01:15:09 +0300

    add asset_id and location_id to task values

--- website_helpdesk_new_fields/models/helpdesk_ticket_convert_wizerd.py
+++ website_helpdesk_new_fields/models/helpdesk_ticket_convert_wizerd.py
@@ -6,6 +6,8 @@ class HelpdeskTicketConvertWizard(models.TransientModel):
 
     @api.model
     def _default_project_id(self):
+        """ Set the default project_id based on the project_task_id field of the active helpdesk ticket. """
+
         ticket_id = self.env.context.get('active_id', False)
 
         if ticket_id:
@@ -14,3 +16,16 @@ class HelpdeskTicketConvertWizard(models.TransientModel):
                 return ticket.project_task_id.id
 
         return False
+
+    @api.model
+    def _get_task_values(self, ticket):
+        """ Override to add asset_id and location_id fields to the task values. """
+
+        task_values = super()._get_task_values(ticket)
+
+        task_values.update({
+            'asset_id': ticket.asset_id.id,
+            'Location_id': ticket.location_id.id,
+        })
+
+        return task_values
