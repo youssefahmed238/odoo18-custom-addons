5cb2601b5270d9021b210e22fbb646a97c5dbb69
Youssef Ahmed <youssef01120646586@gmail.com>
2025-09-29 20:25:20 +0300

    create crm_purchase_links module and initiate manifest

---
+++ crm_purchase_links/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "CRM Purchase Links",
+    "version": "18.0.0.1.0",
+    "author": "Youssef",
+    "depends": ["crm", "purchase"],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
