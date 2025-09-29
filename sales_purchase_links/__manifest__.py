a45fce89792292e0036cb9b3efe2c775b4ca4247
Youssef Ahmed <youssef01120646586@gmail.com>
2025-09-29 16:15:25 +0300

    create new module sales_purchase_links and initiate manifest

---
+++ sales_purchase_links/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Sales Purchase Links",
+    "version": "18.0.0.1.0",
+    "author": "Youssef",
+    "depends": ["sale_management", "purchase"],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
