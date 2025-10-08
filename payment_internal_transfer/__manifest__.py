e22329911d699d918bc119cde66fd62d5f698b1a
Youssef Ahmed <youssef01120646586@gmail.com>
2025-10-08 13:14:11 +0300

    initiate manifest file

---
+++ payment_internal_transfer/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Internal Transfer Payment Method",
+    "author": "Youssef",
+    "version": "18.0.0.1.0",
+    "category": "Accounting",
+    "depends": ["account"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
