fbb96d607e01219c2156fd3f3cc8b9d7d4ed9533
Youssef Ahmed <youssef01120646586@gmail.com>
2025-10-17 16:27:48 +0300

    create module internal_transfer_bank_charge and initiate manifest

---
+++ internal_transfer_bank_charge/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Internal Transfer Bank Charge',
+    'version': '18.0.0.1.0',
+    'author': 'Youssef',
+    'depends': ['account', 'payment_internal_transfer'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
