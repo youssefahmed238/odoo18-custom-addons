7581fcb1b8336f19d637b9276ffd442ef3d18e72
Youssef Ahmed <youssef01120646586@gmail.com>
2025-10-27 11:14:26 +0300

    create search_asset_with_qr module and add manifest file

---
+++ search_asset_with_qr/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Search Asset with QR Code',
+    'version': '18.0.0.1.0',
+    'depends': ['vit_facility_management_config'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
