07e3323487e12ca2431472c56cfdec087ce0f1cc
Youssef Ahmed <youssef01120646586@gmail.com>
2025-11-04 12:57:21 +0200

    create module and init manifest file

---
+++ change_customer_statement_report_logo/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Change Customer Statement Report Logo',
+    'author': 'Youssef',
+    'version': '18.0.0.1.0',
+    'category': 'Accounting',
+    'depends': ['account_reports'],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
