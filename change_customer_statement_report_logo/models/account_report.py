475eb26e9407bfc39c664028a2cd7470721a6878
Youssef Ahmed <youssef01120646586@gmail.com>
2025-11-04 13:26:47 +0200

    add my custom button

---
+++ change_customer_statement_report_logo/models/account_report.py
@@ -0,0 +1,19 @@
+from odoo import models, _
+
+
+class AccountReport(models.Model):
+    _inherit = 'account.report'
+
+    def _init_options_buttons(self, options, previous_options):
+        super(AccountReport, self)._init_options_buttons(options, previous_options)
+
+        if self.custom_handler_model_name == 'account.customer.statement.report.handler':
+            options['buttons'].insert(0, {
+                'name': _('Global Gate PDF'),
+                'sequence': 5,
+                'action': 'export_file',
+                'action_param': 'export_to_pdf',
+                'file_export_type': _('PDF'),
+                'branch_allowed': True,
+                'always_show': True
+            })
