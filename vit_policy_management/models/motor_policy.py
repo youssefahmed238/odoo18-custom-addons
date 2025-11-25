3b66defd3705952329872053395cd2e1692d2370
Youssef Ahmed <youssef01120646586@gmail.com>
2025-11-25 16:53:22 +0200

    add last version

--- vit_policy_management/models/motor_policy.py
+++ vit_policy_management/models/motor_policy.py
@@ -14,7 +14,7 @@ class MotorPolicy(models.Model):
 
     # -------- group 1 -------------
     insurer = fields.Many2one('res.partner', string="Insurer")
-    product = fields.Many2one('policy.product', string="Product", domain=[('category_id', '=', 'motor')])
+    product = fields.Many2one('policy.product', string="Product", domain=[('category_id', '=', 'Motor')])
     customer = fields.Many2one('res.partner', string="Customer")
     business_source_id = fields.Many2one('res.partner', string="Business Source Id")
     kay_account = fields.Char(string="Kay Account")
