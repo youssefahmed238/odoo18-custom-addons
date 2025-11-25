9923a2ccae1a8f5ab6f5497f89c8d00bcb310ecf
Youssef Ahmed <youssef01120646586@gmail.com>
2025-11-25 16:32:18 +0200

    add new module to handle OTP code in portal screen

---
+++ portal_otp/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Portal OTP',
+    'author': 'Youssef Ahmed',
+    'version': '1.0',
+    'category': 'Portal',
+    'depends': ['portal', 'odoo_all_in_one_whatsapp_ent'],
+    'data': [
+        'views/portal_security_inherit.xml',
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
