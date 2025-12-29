960510679aaa6d34a77451e193d79c6c0aaec342
Youssef Ahmed <youssef01120646586@gmail.com>
2025-12-29 13:56:32 +0200

    create module and initiate manifest file

---
+++ stock_thermal_reports/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Stock Thermal Reports',
+    'version': '1.0',
+    'author': 'Youssef Ahmed',
+    'category': 'Inventory',
+    'depends': ['stock'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
