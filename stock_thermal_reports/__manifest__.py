028dd60f7d36268d46c3cedb1824d0db327bc97f
Youssef Ahmed <youssef01120646586@gmail.com>
2025-12-30 14:31:53 +0200

    create reports action

--- stock_thermal_reports/__manifest__.py
+++ stock_thermal_reports/__manifest__.py
@@ -3,7 +3,10 @@
     'version': '1.0',
     'author': 'Youssef Ahmed',
     'category': 'Inventory',
-    'depends': ['stock'],
+    'depends': ['stock', 'thermal_base'],
+    'data': [
+        'reports/stock_report_views.xml',
+    ],
     'installable': True,
     'application': False,
     'auto_install': False,
