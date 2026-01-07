3cadaaaf3f5dcf8c79f7ba27c9fa28bc8385da8b
Youssef Ahmed <youssef01120646586@gmail.com>
2026-01-07 19:07:26 +0200

    initiate manifest

---
+++ stage_state/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Stage State',
+    'version': '1.0',
+    'author': 'Youssef Ahmed',
+    'depends': ['base', 'web'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
