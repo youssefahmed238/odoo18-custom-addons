156e4e0cb3e30355f412a57a57f03dbc062f8b06
Youssef Ahmed <youssef01120646586@gmail.com>
2026-01-07 19:09:48 +0200

    initiate example module

---
+++ stage_state_payment_example/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Stage State Payment Example',
+    'version': '1.0',
+    'author': 'Youssef Ahmed',
+    'depends': ['stage_state', 'account'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
