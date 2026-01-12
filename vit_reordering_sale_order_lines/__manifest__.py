f4f49b6bbd75a713376fdf90675e9d63cfdd0a53
Youssef Ahmed <youssef01120646586@gmail.com>
2026-01-12 19:38:44 +0200

    initiate manifest

---
+++ vit_reordering_sale_order_lines/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Reordering Sale Order Lines',
+    'version': '1.0',
+    'author': 'Youssef Ahmed',
+    'category': 'Sales',
+    'depends': ['sale_management'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
